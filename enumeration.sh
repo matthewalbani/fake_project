44d48d6689cfaf9426ca1fb473653080afef1c8f
Matthew Albani <Matthew.jalbani@gmail.com>
2016-05-16 12:32:12 -0700

    Update enumeration.sh

--- enumeration.sh
+++ enumeration.sh
@@ -1,1 +1,1 @@
-echo "{\"test_scripts\":[\"features/test_stuff.feature\",\"spec/faked_spec.rb\",\"spec/forking_spec.rb\",\"spec/meta_magic_spec.rb\",\"spec/some_class_spec.rb\",\"spec/spec_helper.rb\",\"test/faked_test.rb\"]}"
+return "{\"test_scripts\":[\"features/test_stuff.feature\",\"spec/faked_spec.rb\",\"spec/forking_spec.rb\",\"spec/meta_magic_spec.rb\",\"spec/some_class_spec.rb\",\"spec/spec_helper.rb\",\"test/faked_test.rb\"]}"
