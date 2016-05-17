aafb435218ba93a04500e3835ab071c474a2dd7f
Matthew Albani <Matthew.jalbani@gmail.com>
2016-05-17 14:28:41 -0700

    Update enumeration.sh

--- enumeration.sh
+++ enumeration.sh
@@ -1,1 +1,1 @@
-echo "{\"tests\":[\"features/test_stuff.feature\",\"spec/faked_spec.rb\",\"spec/forking_spec.rb\",\"spec/meta_magic_spec.rb\",\"spec/some_class_spec.rb\",\"spec/spec_helper.rb\",\"test/faked_test.rb\"]}" > ~/test_list.json
+echo "{\"tests\":[\"features/test_stuff.feature\",\"spec/faked_spec.rb\",\"spec/forking_spec.rb\",\"spec/meta_magic_spec.rb\",\"spec/some_class_spec.rb\",\"test/faked_test.rb\"]}" > ~/test_list.json
