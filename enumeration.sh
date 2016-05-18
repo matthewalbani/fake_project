c672ea3a974a4f754acbdcee90702a90fb62ae9a
Matthew Albani <Matthew.jalbani@gmail.com>
2016-05-17 17:28:18 -0700

    Update enumeration.sh

--- enumeration.sh
+++ enumeration.sh
@@ -1,4 +1,4 @@
-echo "{'tests':['features/test_stuff.feature','spec/faked_spec.rb','spec/forking_spec.rb','test/faked_test.rb']}" > ~/test_list.json
+echo '{"tests":["features/test_stuff.feature","spec/faked_spec.rb","spec/forking_spec.rb","test/faked_test.rb"]}' > ~/test_list.json
 env
 echo $SOLANO_PROFILE_NAME
 echo $TDDIUM_CURRENT_BRANCH
